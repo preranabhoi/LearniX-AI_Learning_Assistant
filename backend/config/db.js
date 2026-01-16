8d65dc069a589c3ca58b4523ba237d6ce160419a
Prerana Bhoi <preranabhoi32@gmail.com>
2026-01-16 22:08:24 +0530

    add MongoDB connection utility using Mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const db = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+  }
+};
+
+export default db;
